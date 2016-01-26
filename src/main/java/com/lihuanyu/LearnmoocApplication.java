b12c7db27ef7888ea46386e2477ea7e453fafaa0
skyADMIN <546485299@qq.com>
2016-01-26 13:38:09 +0800

    project init by Spring initialize

---
+++ src/main/java/com/lihuanyu/LearnmoocApplication.java
@@ -0,0 +1,12 @@
+package com.lihuanyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnmoocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnmoocApplication.class, args);
+	}
+}
