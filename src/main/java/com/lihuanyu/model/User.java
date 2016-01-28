ffd5c5ac9053a04839bacab17a9a13b91092a012
skyADMIN <546485299@qq.com>
2016-01-28 22:40:29 +0800

    再改表我会疯的。

--- src/main/java/com/lihuanyu/model/User.java
+++ src/main/java/com/lihuanyu/model/User.java
@@ -1,7 +1,6 @@
 package com.lihuanyu.model;
 
 import javax.persistence.*;
-import java.sql.Date;
 
 /**
  * Created by skyADMIN on 16/1/27.
@@ -20,6 +19,7 @@ public class User {
     private String password;
     private int role_type;
     private int status;
+    private String mail;
     private String major;
     private String academy;
 
@@ -29,13 +29,16 @@ public class User {
         this.id = id;
     }
 
-    public User(String name,String password){
-        this.name = name;
+    public User(String nickname,String password){
+        this.nickname = nickname;
         this.password = password;
+        this.status = 1;
+        this.mail = "123@qq.com";
     }
 
-    public User(String no,String nickname,String avatar,String name,String password,int role_type,int status,String major,String academy){
+    public User(String no,String mail,String nickname,String avatar,String name,String password,int role_type,int status,String major,String academy){
         this.no = no;
+        this.mail = mail;
         this.nickname = nickname;
         this.avatar = avatar;
         this.name = name;
@@ -124,5 +127,13 @@ public class User {
 
     public void setAcademy(String academy) {
         this.academy = academy;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
     }
 }
